aa38ba9fcfa94f038ebd888327bd560484d4eb7a
docre <kutoseemmanuel@gmail.com>
2025-06-19 10:19:57 +0100

    Add files via upload

---
+++ samedi 11.py
@@ -0,0 +1,19 @@
+print("Fuck you petit enculer, on joue à un petit jeu. T'es partant ?")
+print("Oui ou non")
+choisi = input("Entre oui et non, fais ton choix mon petit 🤣😈: ")
+
+if choisi == "oui":
+    print(f"{choisi}: cooool")
+else:
+    print("Bon ! Les règles sont pour faire court : tu choisis deux nombres. Si l'un correspond à celui auquel je pense, t'as 5/10. Si les deux sont ceux auxquels je pense, alors t'as 10/10. Mais au cas contraire, des deux, c'est zéro 0 😂😭.")
+
+nombre = int(input("Entrez un nombre au choix 😈: "))
+
+print("T'en es sûr ?")
+toi = int(input("Choisis un deuxième, mon petit 💀: "))
+
+print("Celui-ci aussi, t'en es sûr ? 😈??")
+if nombre == 30 and toi == 30:
+    print("Nooooon, t'as réussi microbe 😡👿, rejoue et tu verras de quel bois je me chauffe 😈: ")
+else:
+    print("Aaaaaah, t'es nul, ça je te l'avais dit, miserai 😒😠😂😂.")
