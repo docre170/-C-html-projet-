cbab8fc349c39ffa8ccc49caf9ded3a034ea4e18
docre <kutoseemmanuel@gmail.com>
2025-06-23 09:46:33 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer>
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="C:\html projet\index.html">Accueil</a></li>
+                    <li><a href="C:\html projet\accueil\cours.html">Cours et formations</a></li>
+                    <li><a href="C:\html projet\accueil\contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div>
+            <p> 📱 Suivez-nous sur :
+            </p>
+            <p><a href="https://wa.me/+243831996048"><i class="fab fa-whatsapp"></i> Whatsapp</a> |<a href="https://telegram/@Techno_world_Credo"><i class="fab fa-telegram"></i>
+                    Telegram</a> |<a href="github.com/docre170"><i class="fab fa-github"></i> Github</a> </p>
+            <p>
+                Contact :
+            <ol>
+                <li> 🖇️ Email : <a href="mailto:credokutanga170@gmail.com">Techno-world</a></li>
+            </ol>
+            </p>
+        </div>
+        <div>
+            <p> ©️ Techno-world</p>
+        </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+        <div class="intro">
+            <img src="C:\html projet\photo\file_000000009b2461f7845f59f51edef343_conversation_id=6801d0a1-ee4c-8009-a575-a82ca18f77f0&message_id=8d84b211-d521-42cf-b651-c6dc334850ca (1).png"
+                alt="logo" title="Techno-World" width="300px">
+            <h1> Techno-World</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="C:\html projet\codage de donnes\codage de données 1.html">Codage de Données</a></li>
+                <li><a href="../developpement web/généralité_frontend.html">Développement Web</a></li>
+                <li><a href="../python/python.acc.html">Développement Python</a></li>
+                <li><a href="C:\html projet\tecnologie web fondamentale\technologie pour tous acc2.html">fondement web</a></li>
+                <li><a href="../langage c/c1 introduction. html.html">Langage C</a></li>
+            </ul>
+        </nav>
+</header>
